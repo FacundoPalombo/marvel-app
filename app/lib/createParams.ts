bb38dd01e5be6d8d639b66c13c6cae7d6e07ecc1
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2024-08-07 14:58:44 +0200

    feat: fake cache control in 1 day

--- app/lib/createParams.ts
+++ app/lib/createParams.ts
@@ -4,7 +4,20 @@ import { createHash as createHash } from "crypto";
 export default function createParams() {
   const privateKey = process.env.API_PRIVATE_KEY!;
   const apikey = process.env.API_PUBLIC_KEY!;
-  const ts = Date.now().toString();
+  const today = new Date();
+
+  //? Here the marvel api say that this should be a dynamic variable, but i will let this for a while, for practicity on handling the cache automatically.
+  // The funny part, this should set a custom cache control mechanism in 1 day
+  const ts = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDay(),
+    1,
+    44,
+    44
+  )
+    .getTime()
+    .toString();
 
   const fullStr = ts + privateKey + apikey;
 
--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ import styles from "./page.module.css";
 import { getCharacters } from "./services/character";
 import CharactersList from "./components/CharactersList";
 
-import characters from "./fixtures/characters.json";
+// import characters from "./fixtures/characters.json";
 
 export default async function Home({ searchParams }: PageProps) {
   const favorite = searchParams?.favorite;
   const character = searchParams?.character;
-  // const characters: CharactersResponse = await getCharacters(character);
+  const characters: CharactersResponse = await getCharacters(character);
 
   console.log(characters.etag);
   return (
